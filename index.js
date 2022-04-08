c8475df7c04e46d5defc959c870ede53af667738
pavelbalyuk <pavel.balyuk.97@mail.ru>
2022-04-08 11:52:52 +0300

    express + nodemon added

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello 123 123");
+});
+app.listen(3000, () => console.log("server running at port"));
